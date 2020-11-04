25a95df8dfea62f7e4d7dabd13991c5f864f3ab4
Natanael1234 <natanaelnosorio@gmail.com>
2020-11-04 04:33:03 -0300

    Páginas adicionadas no módulo principal

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
+    ProductsPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
