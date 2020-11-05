5304a55bc56370a135120a4c8d5764ad7ae1ee1f
Natanael1234 <natanaelnosorio@gmail.com>
2020-11-04 22:08:14 -0300

    Detecção automática do nome da página em app.component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Router,NavigationEnd, NavigationStart, NavigationError, Event  } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'loja-virtual';
+  currentRoute: string;
+
+  constructor(private router: Router){
+    console.log(router.url);
+
+
+    this.router.events.subscribe((event: Event) => {
+
+      if (event instanceof NavigationEnd) {
+        let tokens = event.urlAfterRedirects.split('/');
+        let route = tokens.length ? tokens[tokens.length - 1] : '';
+        this.currentRoute = route && route.length ? route.charAt(0).toUpperCase() + route.slice(1) : '';
+      }
+
+
+  });
+
+}
+
 }
