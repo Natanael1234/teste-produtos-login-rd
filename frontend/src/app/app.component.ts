c8b82175ebbff3a36fbc0fe3f49aa61e8c5f5919
Natanael1234 <natanaelnosorio@gmail.com>
2020-11-06 06:43:40 -0300

    Separação de código em componentes  e reestruturação do leiaute

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,75 +1,13 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router, NavigationEnd, NavigationStart, NavigationError, Event } from '@angular/router';
-import { LoginService } from './services/login/login.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html', encapsulation: ViewEncapsulation.None,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  /** Nome da página/rota atual. */
-  currentRoute: string;
-  /** Dados do formulário de login. */
-  loginForm = new FormGroup({
-    email: new FormControl('', { validators: [Validators.email, Validators.minLength(1)], updateOn: 'blur' }),
-    password: new FormControl('', { validators: [Validators.minLength(6)], updateOn: 'blur' })
-  });
 
-  /** Se houve falha no login. */
-  loginError: boolean = false;
-  /** Dados do usuário logado. */
-  loginData: { email: string, name: string };
-
-  constructor(private router: Router, public loginService: LoginService) {
-    console.log(router.url);
-
-    // recupera dados do usuário, se este efetuou login anteriormente.
-    this.loginService.getLogin().then((loginData) => {
-      this.loginData = loginData;
-    });
-
-    // observa mudanças na rota para mudar o nome da página no cabeçalho
-    this.router.events.subscribe((event: Event) => {
-      if (event instanceof NavigationEnd) {
-        let tokens = event.urlAfterRedirects.split('/');
-        let route = tokens.length ? tokens[tokens.length - 1] : '';
-        this.currentRoute = route && route.length ? route.charAt(0).toUpperCase() + route.slice(1) : '';
-      }
-    });
-  }
-
-  /** Efetua login com os dados do formulário de login. */
-  async login(event: any) {
-    // evita o recarregamento da página
-    event.preventDefault();
-    // efetua login na API
-    let loginData = await this.loginService.login(
-      this.loginForm.controls.email.value,
-      this.loginForm.controls.password.value
-    );
-    if (loginData) {
-      this.loginData = loginData;
-      this.resetLogin();
-    } else {
-      this.loginData = null;
-      this.loginError = true;
-    }
-  }
-
-  /** Limpa o formulário de login. */
-  resetLogin() {
-    this.loginForm.controls.email.reset();
-    this.loginForm.controls.password.reset();
-    this.loginForm.reset();
-    this.loginError = false;
-  }
-
-  logout() {
-    this.loginService.logout();
-    this.loginData = null;
-    this.resetLogin();
-  }
+  constructor(private router: Router) { }
 
   /** Navega para a tela inicial. */
   navigateToHome() {
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,12 +7,28 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductsPageComponent } from './pages/products-page/products-page.component';
 import { CartPageComponent } from './pages/cart-page/cart-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { CartItemComponent } from './components/cart-item/cart-item.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { CartTotalComponent } from './components/cart-total/cart-total.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsPageComponent,
-    CartPageComponent
+    CartPageComponent,
+    FooterComponent,
+    HeaderComponent,
+    BannerComponent,
+    LoginFormComponent,
+    CartItemComponent,
+    ProductCardComponent,
+    PageHeaderComponent,
+    CartTotalComponent
   ],
   imports: [
     BrowserModule,
